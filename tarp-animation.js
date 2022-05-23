cd1a91a8f0bf3fbc9c8c1ac1a32449d10bcc7e4d
bryku <drburnett@mail.com>
2022-05-22 22:22:27 -0700

    Update tarp-animation.js

--- tarp-animation.js
+++ tarp-animation.js
@@ -1,28 +1,39 @@
-let tarpAnimation = {};
-	tarpAnimation.intervals = {};
-	tarpAnimation.createIntervalKey = function(){
-		return new Date().getUTCMilliseconds()
-	};
-	tarpAnimation.move = function(startPosition, endPosition, speed = 1){
-		let key = this.createIntervalKey;
-		this.intervals[key] = setInterval((startPosition, endPosition, speed, key)=>{
-			console.log('interval');
-			if(startPosition.x < endPosition.x){
-				startPosition.x+= speed;
-				if(startPosition.x > endPosition.x){startPosition.x = endPosition.x}
-			}else if(startPosition.x > endPosition.x){
-				startPosition.x-= speed;
-				if(startPosition.x < endPosition.x){startPosition.x = endPosition.x}
-			}
-			if(startPosition.y < endPosition.y){
-				startPosition.y+= speed;
-				if(startPosition.y > endPosition.y){startPosition.y = endPosition.y}
-			}else if(startPosition.y > endPosition.y){
-				startPosition.y-= speed;
-				if(startPosition.y < endPosition.y){startPosition.y = endPosition.y}
-			}
-			if(startPosition.x == endPosition.x && startPosition.y == endPosition.y){
-				clearInterval(this.intervals[key])
-			}
-		}, 16, startPosition, endPosition, speed, key);
-	};
+let tarpAnimation = {
+    intervals: {},
+    createIntervalKey: function(){
+        return new Date().getUTCMilliseconds()
+    },
+};
+tarpAnimation.move = function(startPosition, endPosition, speed = 1){
+    let key = this.createIntervalKey;
+    this.intervals[key] = setInterval((startPosition, endPosition, speed, key)=>{
+        if(startPosition.x < endPosition.x){
+            startPosition.x+= speed;
+            if(startPosition.x > endPosition.x){startPosition.x = endPosition.x}
+        }else if(startPosition.x > endPosition.x){
+            startPosition.x-= speed;
+            if(startPosition.x < endPosition.x){startPosition.x = endPosition.x}
+        }
+        if(startPosition.y < endPosition.y){
+            startPosition.y+= speed;
+            if(startPosition.y > endPosition.y){startPosition.y = endPosition.y}
+        }else if(startPosition.y > endPosition.y){
+            startPosition.y-= speed;
+            if(startPosition.y < endPosition.y){startPosition.y = endPosition.y}
+        }
+       if(startPosition.x == endPosition.x && startPosition.y == endPosition.y){
+            clearInterval(this.intervals[key])
+       }
+   }, 16, startPosition, endPosition, speed, key);
+};
+tarpAnimation.movePoint = function(point, value){
+    if(point.d == 'down'){
+        point[value]+= point.speed || 1;
+        if(point[value] > 90){point[value] = 90; point.d = 'up'}
+    }else if(point.d == 'up'){
+        point[value]-= point.speed || 1;
+        if(point[value] < 60){point[value] = 60; point.d = 'down'}
+    }else{
+        point.d = 'down'
+    }
+};
